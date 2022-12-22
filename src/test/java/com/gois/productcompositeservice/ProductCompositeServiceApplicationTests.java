b5380a343c271acf869c641bb0cb1516cc0a466a
Gustavo Meneses Gois <gustavo.gois@gmail.com>
2022-12-22 00:23:49 +0000

    multi-module + api project + util project

---
+++ src/test/java/com/gois/productcompositeservice/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gois.productcompositeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
